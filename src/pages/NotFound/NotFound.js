782c15e9fc20ff8e42d7b4c45ca9cd34fc24596d
Saifullah Mansur <58740153+saikot-cse@users.noreply.github.com>
2022-05-22 19:12:06 +0600

    Notfound page added

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,5 +1,22 @@
+// import { Helmet } from "react-helmet-async";
+import { useNavigate } from "react-router-dom";
+import pageNotFound from "../../assets/pageNotFound.jpg";
 export const NotFound = () => {
+  const navigate = useNavigate();
+  const navigateToHome = () => {
+    navigate("/");
+  };
   return (
-    <div>NotFound</div>
-  )
-}
+    <div>
+      {/* <Helmet>
+        <title>404 Page Not Found</title>
+      </Helmet> */}
+      <img className="mx-auto d-block mt-3 rounded img-fluid" src={pageNotFound} alt="" />
+      <button onClick={navigateToHome} className="font-extrabold btn btn-primary mx-auto block my-3 text-white">
+        Go back to Home
+      </button>
+    </div>
+  );
+};
+
+
